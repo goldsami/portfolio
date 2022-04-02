5aba44c4aa63c3b6c9e20a2c173caf69914a7093
goldsami <zolotarenkomax@gmail.com>
2022-04-02 22:11:07 +0300

    feat: social media links removed from footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,18 +4,12 @@ import {DataService} from "../services";
 
 export default function Footer() {
   const {name} = DataService.me()
-  const {github, linkedin, telegram} = DataService.contacts()
 
   return (
     <footer className="footer pb-6">
       <div className="content columns is-mobile">
         <div className="column is-half">
           Developed by <b style={{whiteSpace: 'nowrap'}}>{name}</b>
-        </div>
-        <div className="column is-half has-text-right">
-          <Icon icon={Icons.github} link={github} />
-          <Icon icon={Icons.linkedin} link={linkedin} />
-          <Icon icon={Icons.telegram} link={telegram} />
         </div>
       </div>
     </footer>
